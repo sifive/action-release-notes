cad23fb782b7968ca3deddcfda0adaa2ebc84eb5
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-05-04 11:16:11 -0700

    Render template
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- create-release-notes.py
+++ create-release-notes.py
@@ -3,10 +3,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import git
+import jinja2
 import sys
 
+def parse_args(argv):
+    if len(argv) < 3:
+        sys.stderr.write("ERROR: 'release' is a required argument")
+        sys.exit(1)
+
+    project_name = argv[1]
+
+    release = argv[2]
+
+    output_path = None
+    if len(argv) >= 4:
+        output_path = argv[3]
+        print("Will generate output in {}".format(output_path))
+
+    return project_name, release, output_path
+
+
 def get_repo():
-    repo = git.Repo()
+    return git.Repo()
+
+
+def get_template():
+    env = jinja2.Environment(
+        loader=jinja2.PackageLoader(__name__, "templates"),
+        trim_blocks=True, lstrip_blocks=True,
+    )
+    return env.get_template("notes.md")
+
 
 def get_release_commit(repo, release_tag):
     tags = [t for t in repo.tags if t.name == release_tag]
@@ -16,26 +43,32 @@ def get_release_commit(repo, release_tag):
     print("Tag {} does not exist, using HEAD commit {}".format(release_tag, repo.head.commit.hexsha))
     return repo.head.commit
 
+
 def get_last_release(repo, release_commit):
     pass
 
+
 def main(argv):
+    project_name, release, output_path = parse_args(argv)
+
     repo = get_repo()
-
-    if len(argv) < 2:
-        sys.stderr.write("ERROR: 'release' is a required argument")
-        sys.exit(1)
-
-    release = argv[1]
-    print("Generating release notes for release {}")
-
-    if len(argv) >= 3:
-        output_path = argv[2]
-        print("Will generate output in {}".format(output_path))
-
+    
     release_commit = get_release_commit(repo, release)
 
-    print("::set-output name=release-notes::Release {} points at commit {}".format(release, release_commit))
+    values = {
+        'project_name': project_name,
+        'release': release,
+    }
+
+    release_notes = get_template().render(values)
+
+    if output_path != None:
+        with open(output_path, 'w') as output_file:
+            output_file.write(release_notes)
+
+    release_notes.replace("\n", "%0A")
+
+    print("::set-output name=release-notes::{}".format(release_notes))
 
 if __name__ == '__main__':
     main(sys.argv)
---
+++ templates/notes.md
@@ -0,0 +1,14 @@
+# Release notes for {{ project_name }} {{ release }}
+## Statistics since {{ last_release }}
+ - {{ num_commits }} commits
+ - {{ stats }}
+
+## Authors
+{% for author in authors %}
+ - {{ author }}
+{% endfor %}
+
+## Merge History
+{% for merge in merges %}
+{{ merge.shorthash }} {{ merge.description }}
+{% endfor %}
